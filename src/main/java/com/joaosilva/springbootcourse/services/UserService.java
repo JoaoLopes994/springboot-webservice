73fee7e519d57fd99c0e0779ebbc4c2e9ba05a12
João Lopes <joaolsilva994@gmail.com>
2021-05-28 12:28:49 -0300

    Camada de serviço e registrando componentes

---
+++ src/main/java/com/joaosilva/springbootcourse/services/UserService.java
@@ -0,0 +1,27 @@
+package com.joaosilva.springbootcourse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaosilva.springbootcourse.entities.User;
+import com.joaosilva.springbootcourse.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User finById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
