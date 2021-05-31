f25b47fc1143166db1557671c605393ff9a39796
João Lopes <joaolsilva994@gmail.com>
2021-05-31 11:04:26 -0300

    Associação de muitos para muitos entre Products e OrderItem

--- src/main/java/com/joaosilva/springbootcourse/entities/Product.java
+++ src/main/java/com/joaosilva/springbootcourse/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_products")
@@ -26,6 +29,9 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String urlImg;
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}
@@ -87,6 +93,15 @@ public class Product implements Serializable {
 		return categories;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
