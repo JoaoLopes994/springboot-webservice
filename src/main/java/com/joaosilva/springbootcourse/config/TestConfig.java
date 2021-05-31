73db6a3f13b4550860af71c2bec6edb15aac947e
João Lopes <joaolsilva994@gmail.com>
2021-05-31 12:04:33 -0300

    Entidade Payment criada com associação um para um

--- src/main/java/com/joaosilva/springbootcourse/config/TestConfig.java
+++ src/main/java/com/joaosilva/springbootcourse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.joaosilva.springbootcourse.entities.Category;
 import com.joaosilva.springbootcourse.entities.Order;
 import com.joaosilva.springbootcourse.entities.OrderItem;
+import com.joaosilva.springbootcourse.entities.Payment;
 import com.joaosilva.springbootcourse.entities.Product;
 import com.joaosilva.springbootcourse.entities.User;
 import com.joaosilva.springbootcourse.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z") , o3);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
